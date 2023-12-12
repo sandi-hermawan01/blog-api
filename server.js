e6bd27a8c61e5cd03bfe558b1d69ec76c847b6e7
sandi-hermawan01 <sandihermawanf01@gmail.com>
2023-12-12 18:17:27 +0700

    update vercel0.0.2

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ import Comment from "./Schema/Comment.js";
 const server = express();
 const PORT = "process.env.PORT || 3003";
 const slatRounds = 10; // slat rounds for bcryptjs
+const cors = require("cors");
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
