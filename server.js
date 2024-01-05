a75cd091b07563c9716125e9e4ce395397b6018b
sandi-hermawan01 <sandihermawanf01@gmail.com>
2024-01-05 20:08:02 +0700

    test update

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ mongoose.connect(process.env.DB_LOCATION_LINK, {
 // initializing the firebaserun
 
 // var admin = require("firebase-admin");
-// var serviceAccount = require("./blog-publisher-v2-firebase-adminsdk-eaofk-05e7fe5acc.json");
-// // import serviceAccount from "./blog-publisher-v2-firebase-adminsdk-eaofk-05e7fe5acc.json" assert { type: "json" };
+// var serviceAccount = require(serviceAccountLocation);
+import serviceAccount from "./blog-publisher-v2-firebase-adminsdk-eaofk-261b8f5b31.json" assert { type: "json" };
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // middlewares
 server.use(express.json()); // enable JSON sharing
